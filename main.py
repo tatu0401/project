9282edfaef260705a7673857df3e3a06aca40541
Tatiana Vitalievna <tatiatiav@yandex.ru>
2026-05-25 18:35:47 +0300

    Подключен модуль utils

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-
+from utils import is_valid_number
 def main():
     grades=[]
     n=None
 
     while not isinstance(n,int):
         value=input("Введите количество предметов")
-        if is_valid_number(value)
+        if is_valid_number(value):
+            n=int(float(value))
+        else:
+            print("Некорректный ввод, повторите попытку")
